b534b65082b68a98c7b35fd0b7cd6b0355d3d9c5
akghosh111 <ghoshkiran2017@gmail.com>
2025-02-17 19:36:15 +0530

    full page screenshot

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const puppeteer = require("puppeteer");
 async function start() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://google.com");
-    await page.screenshot({path: "googless.png"});
+    await page.goto("https://en.wikipedia.org/wiki/JavaScript");
+    await page.screenshot({path: "jswiki.png", fullPage:true});
     await browser.close();
 }
 
