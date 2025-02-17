186b84bd26dc45ae73dadcb12ff9843300a1bff6
akghosh111 <ghoshkiran2017@gmail.com>
2025-02-17 20:18:50 +0530

    clicking a button

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ async function start() {
     })
     await fs.writeFile("names.txt", names.join("\r\n"));
 
+    await page.click("#clickme")
+    const clickedData = await page.$eval("#data", el =>el.textContent)
+    console.log(clickedData)
+
     const photos = await page.$$eval("img", (imgs) => {
         return imgs.map(x => x.src)
     })
